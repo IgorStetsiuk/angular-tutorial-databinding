2c3854242758e07c43d22ac565a4b1ac420778f7
Ihor Stetsiuk <istetsiuk@griddynamics.com>
2019-04-01 15:59:16 +0300

    Components
    Splitting components

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-cockpit",
+  templateUrl: "./cockpit.component.html"
+  // styleUrls: ['./cockpit.component.scss']
+})
+export class CockpitComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
